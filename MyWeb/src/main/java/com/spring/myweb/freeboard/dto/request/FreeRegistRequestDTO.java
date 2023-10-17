974f28200a6dcba724d5c817dbd54138aab09818
jisu <parkjisu0107@gmail.com>
2023-10-17 18:29:54 +0900

    231017 페이징알고리즘, Rest통신연습, id중복확인fetch학습

---
+++ MyWeb/src/main/java/com/spring/myweb/freeboard/dto/request/FreeRegistRequestDTO.java
@@ -0,0 +1,21 @@
+package com.spring.myweb.freeboard.dto.request;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class FreeRegistRequestDTO {
+
+	private String title;
+	private String content;
+	private String writer;
+}
---
+++ MyWeb/src/main/java/com/spring/myweb/user/service/UserService.java
@@ -0,0 +1,52 @@
+package com.spring.myweb.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.myweb.user.dto.UserJoinRequestDto;
+import com.spring.myweb.user.entity.User;
+import com.spring.myweb.user.mapper.IUserMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final IUserMapper mapper;
+
+	public int idCheck(String account) {
+		return mapper.idCheck(account);
+	}
+
+	public void join(UserJoinRequestDto dto) {
+		// dto를 entity로 변환
+		User user = User.builder()
+				.userId(dto.getUserId())
+				.userPw(dto.getUserPw())
+				.userName(dto.getUserName())
+				.userPhone1(dto.getUserPhone1())
+				.userPhone2(dto.getUserPhone2())
+				.userEmail1(dto.getUserEmail1())
+				.userEmail2(dto.getUserEmail2())
+				.addrBasic(dto.getAddrBasic())
+				.addrDetail(dto.getAddrDetail())
+				.addrZipNum(dto.getAddrZipNum())
+				.build();
+		mapper.join(user);
+	}
+
+	public void login(String userId) {
+		mapper.login(userId);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
